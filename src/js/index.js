f22889f3a20e3849df242e74731f8d8f40c89670
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2021-08-08 17:13:35 +0300

    all section

--- src/js/index.js
+++ src/js/index.js
@@ -15,3 +15,46 @@ function testWebP(callback) {
         document.querySelector('body').classList.add('no-webp');
     }
 });
+
+tns({
+    container: '.slider',
+    items: 3,
+    gutter: 32,
+    autoplay: true,
+    autoplayButton: false,
+    autoplayButtonOutput: false,
+    autoplayResetOnVisibility: false,
+    nav: false,
+    mouseDrag: true,
+    freezable: true,
+    nested: "outer",
+    loop: false,
+    prevButton: '.slider-prev',
+    nextButton: '.slider-next'
+});
+
+let imgActiveArrow = 'img/svg/slider-arrow-red.svg';
+let imgNoActiveArrow = 'img/svg/slider-arrow.svg';
+
+console.log(button_next.getAttribute('aria-disabled'))
+console.log(button_prev.getAttribute('aria-disabled'))
+
+button_prev.addEventListener('click', function(){
+    console.log(button_prev.getAttribute('aria-disabled'))
+    if(button_prev.getAttribute('aria-disabled') == 'true'){
+        document.querySelector('#button_prev img').setAttribute('src', imgNoActiveArrow)
+        document.querySelector('#button_prev source').setAttribute('srcset', imgNoActiveArrow)
+
+        document.querySelector('#button_next img').setAttribute('src', imgActiveArrow)
+    }
+})
+
+button_next.addEventListener('click', function(){
+    console.log(button_next.getAttribute('aria-disabled'))
+    if(button_next.getAttribute('aria-disabled') == 'true'){
+        document.querySelector('#button_next img').setAttribute('src', imgNoActiveArrow)
+
+        document.querySelector('#button_prev img').setAttribute('src', imgActiveArrow)
+        document.querySelector('#button_prev source').setAttribute('srcset', imgActiveArrow)
+    }
+})
